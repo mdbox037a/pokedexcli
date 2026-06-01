05545e2772261a1fd8a7d18c3c5bd71212312c57
Matt Box <mdbox037a@protonmail.com>
2026-05-31 21:34:39 -0400

    define test cases for cleanInput function

---
+++ repl_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "this space this	tab",
+			expected: []string{"this", "space", "this", "tab"},
+		},
+		{
+			input:    "under_score and end     ",
+			expected: []string{"under_score", "and"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf(
+				"test failed: actual slice length [%d] and expected slice length [%d] do not match", len(actual), len(c.expected))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("test failed: actual word [%s] does not match expected word [%s]", word, expectedWord)
+			}
+		}
+	}
+}
