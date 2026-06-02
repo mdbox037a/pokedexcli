4c3210c7bbf7f0ab60744d3026bc88f5bcd46909
Matt Box <mdbox037a@protonmail.com>
2026-06-02 06:13:03 -0400

    move application logic from main.go to repl.go

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		clean := make([]string, 0)
-		if input != "" {
-			clean = cleanInput(input)
-		}
-	}
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,25 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+		input := reader.Text()
+		clean := make([]string, 0)
+		if input != "" {
+			clean = cleanInput(input)
+			fmt.Printf("Your command was %s\n", clean[0])
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
