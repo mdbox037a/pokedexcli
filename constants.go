8861ba6070de12d0deae5aa90d6f682a83876115
Matt Box <mdbox037a@protonmail.com>
2026-06-04 23:03:34 -0400

    move locationArea struct to constants file

--- constants.go
+++ constants.go
@@ -15,3 +15,13 @@ type state struct {
 	next      *string
 	previous  *string
 }
+
+type locationArea struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
