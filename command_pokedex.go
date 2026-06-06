060bd617c669481edd7785f8a5ed7fa7856564d0
Matt Box <mdbox037a@protonmail.com>
2026-06-06 15:51:40 -0400

    implement core logic to print contents of user pokedex

---
+++ command_pokedex.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandPokedex(currentState state, args ...string) error {
+	if len(currentState.pokedex) == 0 {
+		fmt.Println("No pokemon caught yet")
+		return nil
+	}
+
+	fmt.Println("Your Pokedex:")
+	for p, _ := range currentState.pokedex {
+		fmt.Printf(" - %s\n", p)
+	}
+	return nil
+}
