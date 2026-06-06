869ecafa491a081d44f50b191d76921e0457d230
Matt Box <mdbox037a@protonmail.com>
2026-06-06 15:37:34 -0400

    implement core logic to handle printing caught pokemon information

--- command_inspect.go
+++ command_inspect.go
@@ -6,5 +6,29 @@ import (
 )
 
 func commandInspect(currentState *state, args ...string) error {
-	//
+	if len(args) == 0 {
+		return errors.New("Error: please supply the name of a caught pokemon to inspect")
+	}
+	target := args[0]
+	if _, caught := currentState.pokedex[target]; !caught {
+		fmt.Println("you have not caught that pokemon")
+		return nil
+		// not returning an error because user cannot query their own pokedex yet
+	}
+
+	p := currentState.pokedex[target]
+	fmt.Printf("Name: %s\n", p.Name)
+	fmt.Printf("Height: %s\n", p.Height)
+	fmt.Printf("Weight: %s\n", p.Weight)
+
+	fmt.Println("Stats:")
+	for _, s := range p.Stats {
+		fmt.Printf("  -%s: %d\n", s.Stat.Name, s.BaseStat)
+	}
+
+	fmt.Println("Types:")
+	for _, t := range p.Types {
+		fmt.Printf("  -%s: %d\n", t.Type.Name)
+	}
+	return nil
 }
